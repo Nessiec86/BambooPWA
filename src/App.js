b7444c282aa9306209d71071fb99c9f0d773488d
Nessiec86 <nessc86@gmail.com>
2019-09-15 22:26:04 +0200

    fixed router

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import NotFound from './Components/NotFound';
+
+class App extends Component {
+  
+  render () {  
+    return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path='*' exact={true} component={NotFound} />
+      </Switch>
+    );
+  }
+}
+
+export default App;
